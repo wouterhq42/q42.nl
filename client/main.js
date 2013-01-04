cab4811601cac67154a2787a0f42c7e16839b0ac
Rahul Choudhury <rahul@q42.nl>
2013-01-04 11:23:19 +0100

    Attach behaviour when re-rendering template instead of stacking Meteor.defer

--- client/main.js
+++ client/main.js
@@ -6,9 +6,11 @@ Meteor.startup(function () {
 Template.body.content = function() {
   var page = Session.get("page") || "home";
   var template = Template[page] || Template["error404"];
-  reattachBehavior();
   return template();
 };
+Template.body.rendered = function() {
+  reattachBehavior();
+}
 
 Template.body.viewRendersHeader = function() {
   var page = Session.get("page") || "home";
@@ -27,33 +29,28 @@ function handleLinkClicks() {
 }
 
 function reattachBehavior() {
-  Meteor.defer(handleLinkClicks);
-  Meteor.defer(homepageShowreel);
-  Meteor.defer(addPolaroidFunctionality);
-  Meteor.defer(qsausInit);
-  Meteor.defer(koffieteller);
-  Meteor.defer(codeteller);
+  handleLinkClicks();
+  homepageShowreel();
+  addPolaroidFunctionality();
+  qsausInit();
+  koffieteller();
+  codeteller();
 
   // fade in new page
-  Meteor.defer(function() {
-    $($("section")[0]).addClass("show");
-    $("#homecontent").addClass("show");
-  });
+  $($("section")[0]).addClass("show");
+  $("#homecontent").addClass("show");
 
   // scroll to top of page
-  Meteor.defer(function() {
-    window.scrollTo(0,0);
-  });
+  window.scrollTo(0,0);
 
   // facebook widget
-  Meteor.defer(function() {
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/nl_NL/all.js#xfbml=1&appId=292443547438127";
-      fjs.parentNode.insertBefore(js, fjs);
-    } (document, 'script', 'facebook-jssdk'));
-  });
-  Meteor.defer(resizeFBwidget);
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/nl_NL/all.js#xfbml=1&appId=292443547438127";
+    fjs.parentNode.insertBefore(js, fjs);
+  } (document, 'script', 'facebook-jssdk'));
+
+  resizeFBwidget();
 }
