e0c83a6b66326bf3b0625e79b5470c6cc7628b8b
Rahul Choudhury <rahul@q42.nl>
2013-03-29 01:05:14 +0100

    Make sure the header looks nice even if we don't get any lights from the API

--- client/lamps.js
+++ client/lamps.js
@@ -1,18 +1,23 @@
 var Colors = new Meteor.Collection("colors");
 
 function updateLightbar() {
-  var colors = _.map(Colors.find().fetch(), function(doc) { return "#" + doc.hex; });
-  console.log("Draw colors", colors);
+  var colors = _.map(Colors.find().fetch(), function(doc) {
+    if (doc.hex) return "#" + doc.hex;
+    else return null;
+  });
+  colors = _.reject(colors, function(c) { return !c; });
+  if (colors.length)
+    console.log("Draw colors", colors);
 
-  var handcraftCornerLight = colors[9]; //12
-  var stefOfficeLight = colors[0];
-  var rijksmuseumTeamLight = colors[4];
-  var cynthiaDeskLight = colors[23];
-  var _9292Light = colors[26];
-  var kitchenLight = colors[14];
+  var handcraftCornerLight = colors[9] || "#9fc"; //12
+  var stefOfficeLight = colors[0] || "#c9f";
+  var rijksmuseumTeamLight = colors[4] || "#f66";
+  var cynthiaDeskLight = colors[23] || "#cf9";
+  var _9292Light = colors[26] || "#9cf";
+  var kitchenLight = colors[14] || "#9cf";
 
   colors = [handcraftCornerLight, stefOfficeLight, rijksmuseumTeamLight, cynthiaDeskLight, _9292Light];
-  if (Colors.find().count() == 29) // THERE ARE 29 LIGHTS!!!
+  if (1 || Colors.find().count() == 29) // THERE ARE 29 LIGHTS!!!
     $("#header").css("background", "-webkit-linear-gradient(left, " + colors + ")");
 }
 
