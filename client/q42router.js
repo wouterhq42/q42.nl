6ec8c76d7436734cb2eb73ba131ac71fcd1810fa
Rahul Choudhury <rahul@q42.nl>
2013-03-30 17:35:43 +0100

    Set up 301 redirect to remove "www." from domain

--- client/q42router.js
+++ client/q42router.js
@@ -9,6 +9,12 @@ var Q42Router = Backbone.Router.extend({
     ":page": "main"
   },
   main: function (page) {
+    // forward to domain without www. prefix
+    if (window.location.hostname.indexOf("www") == 0) {
+      window.location.href = window.location.href.replace("http://www.", "http://");
+      return;
+    }
+
     if (page)
       page = page.split("#")[0].split("?")[0];
 
---
+++ server/forward_domain.js
@@ -0,0 +1,15 @@
+
+// 301 redirect to remove www. from domain
+Meteor.startup(function() {
+  Meteor.Router.add("*", function() {
+    var host = this.request.headers.host;
+    var url = this.request.url;
+    var fullURL = "http://" + host + url;
+
+    if (host.indexOf("www") == 0) {
+      return [301, fullURL.replace("www.", "www")];
+    }
+
+    return false;
+  });
+});
