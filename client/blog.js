17be9531a0d0ba4693d349b5d7ad5187fcb9fe34
Rahul Choudhury <rahul@q42.nl>
2013-04-13 01:27:52 +0200

    Async facebook load

--- client/blog.js
+++ client/blog.js
@@ -49,6 +49,7 @@ Template.en_blogpost.rendered = Template.blogpost.rendered = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
+    js.async = true;
     js.src = "//connect.facebook.net/nl_NL/all.js#xfbml=1&appId=292443547438127";
     fjs.parentNode.insertBefore(js, fjs);
   } (document, 'script', 'facebook-jssdk'));
