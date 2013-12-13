fcf6f01b7d5de2a644f55c2db51fb2343be91152
Rahul Choudhury <rahul@q42.nl>
2013-12-13 20:46:56 +0100

    Update deploy script to send assets to the CDN Update readme

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,13 @@
 #        exit 1;
 #fi
 
+# Publish all assets to the CDN rather than serving them from meteor.com
+cd public
+gsutil -m cp -R * gs://static.q42.nl
+cd ..
+
+# Deploy the site to two different Meteor domains
+# because that's the way we make sure we have an English and a Dutch website :-)
 echo "Deploying to q42.nl..."
 echo
 meteor deploy q42.nl < password.txt
