a30bf11dce816a4dcc6ebc83614424e0e9dbec4a
Rahul Choudhury <rahul@q42.nl>
2013-12-23 14:08:23 +0100

    Remove redundant code

--- server/api.js
+++
@@ -1,6 +0,0 @@
-/*Meteor.Router.add("/updateLightbar", "GET", function() {
-  this.response.setHeader("Access-Control-Allow-Origin", "http://huelandsspoor.nl");
-  console.log("Received request from huelandsspoor. Updating...")
-  updateLightbar();
-  return 200;
-});*/
