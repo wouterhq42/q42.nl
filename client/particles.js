084c45713be778f8c816decb67931a535ceedf57
Rahul Choudhury <rahul@q42.nl>
2014-05-26 12:46:34 +0200

    Add particles to header

---
+++ client/particles.js
@@ -0,0 +1,68 @@
+Template.header.rendered = function() {
+  function c(n) { if (!n) n = 255; return ~~(Math.random() * n); }
+  function rgba(r, g, b, a) { return "rgba("+r+","+g+","+b+","+a+")"; }
+  function rc(n, a) { return rgba(c(n), c(n), c(n), a); }
+  function rp() { return ~~((Math.random()*2-1)*250); }
+  function rr() { return ~~((Math.random()*2-1)*20); }
+  function between(a, b) { return Math.max(a, ~~(Math.random()*b)); }
+
+  function generateparticles(numparticles) {
+    var particles = $(".particle");
+    $("#particles").html("");
+    _.times(numparticles, function(n){ generateparticle(n) });
+    $("head").append($style);
+  }
+
+  function generateparticle(i) {
+    var $particle = $("<div/>")
+    $particle.addClass("particle");
+    $particle.attr("id", "particle" + i);
+
+    var delay = ~~(Math.random()*4) + "s";
+    var duration = between(10, 60) + "s";
+
+    $particle.css("-webkit-animation", "float-particle"+i+" "+duration+" "+delay+" ease-in-out infinite alternate");
+
+    var dim = ~~(Math.random() * 5);
+    $particle.css("width", dim + "px");
+    $particle.css("height", dim + "px");
+
+    var color = rc(0, .8);
+    $particle.css("backgroundColor", color);
+
+    $particle.css("boxShadow", "0 0 " + ~~(Math.random()*20) + "px " + color);
+    $particle.css("padding", ~~(Math.random()*20) + "px");
+    $particle.css("borderRadius", between(10, 20) + "px");
+
+    var x = ~~(Math.random() * $(window).width());
+    var y = rp();
+    var z = -rp();
+    var rx = rr();
+    var ry = rr();
+    var rz = rr();
+
+    $particle.css("-webkit-transform", "translateX(" + x + "px) translateY(" + y + "px) translateZ(" + z +
+      "px) rotateX(" + rx + "deg) rotateY(" + ry + "deg) rotateZ(" + rz + "deg)");
+    $("#particles").append($particle);
+
+    var dx = between(40, 100);
+    dx = Math.random() < 0.5 ? dx : -dx;
+
+    var rdx = between(5, 20);
+    rdx = Math.random() < 0.5 ? rdx : -rdx;
+
+    var from = "-webkit-transform: translate(-50%, -50%) translate3d("+x+"px, "+y+"px, "+z+"px) rotateX("+rx+"deg) rotateY("+ry+"deg) rotateZ("+rz+"deg) scale(1); opacity: 1;";
+    var to = "-webkit-transform: translate(-50%, -50%) translate3d("+(x+dx)+"px, "+(y+dx)+"px, "+(z+dx)+"px) rotateX("+(rx+rdx)+"deg) rotateY("+(ry+rdx)+"deg) rotateZ("+(rz+rdx)+"deg) scale(1); opacity: 0";
+    var wka = "\n@-webkit-keyframes float-particle"+i+" { from {" + from + "} to {" + to + "} }";
+
+    $style.html($style.html() + wka);
+  }
+
+  var $style = $("<style/>");
+  $style.attr('type', 'text/css');
+  $style.html("");
+
+  var numparticles = 42;
+  generateparticles(numparticles);
+
+};
