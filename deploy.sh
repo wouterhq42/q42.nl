8bb21d95b29285334cf7dad4dd8c24d45da251cd
Lukas van Driel <lukas@q42.nl>
2015-05-12 10:45:47 +0200

    ok that doesn't work :(

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo "------ Begin Q42.nl and Q42.com deploy! ------"
 # Use git flow to tag this release with the current date (eg. 2015-03-21)
 echo "--- Creating a release..."
 git flow release start $NOW
-git flow release finish -m 'Deploying' $NOW
+git flow release finish $NOW
 echo "--- New release $NOW created."
 echo
 
