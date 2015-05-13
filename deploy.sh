8603426abdc339621fb877314a8ef224d4df3a80
Rahul Choudhury <rchoudhury@gmail.com>
2015-05-13 10:30:02 -0700

    Update deploy.sh to point to scalingo Remove manual /public folder moving

--- deploy.sh
+++ deploy.sh
@@ -17,39 +17,17 @@ gsutil -m rsync -r -d public gs://static.q42.nl || exit 1
 echo "--- Done with CDN."
 echo
 
-# Move the /public folder out of the Meteor folder before deploying
-# So that the upload size is dramatically smaller
-# (since we already pushed it to the CDN)
-echo "--- Excluding public folder from deploy..."
-rm -rf $TMPDIR/q42nl-deploy-public
-mkdir $TMPDIR/q42nl-deploy-public
-mv public $TMPDIR/q42nl-deploy-public
-echo
-
 # Deploy the site to two different Meteor domains
 # so we have an English and a Dutch website :-)
-# Also, don't deploy the templates of the EN site to the NL site and vice versa
 echo "--- Deploying to q42.nl..."
-echo "--- Excluding EN templates..."
-rm -rf $TMPDIR/q42nl-deploy-EN
-mkdir $TMPDIR/q42nl-deploy-EN
-mv views/en $TMPDIR/q42nl-deploy-EN
-echo
 # meteor deploy q42.nl --settings config/settings.json
-scalingo push scalingo-nl master
+git push scalingo-nl master
 echo "--- Done deploying to q42.nl. Refresh your browser!"
-echo "--- Restoring EN templates..."
-mv $TMPDIR/q42nl-deploy-EN/* views
 
 echo "--- Deploying to q42.com..."
 # meteor deploy q42.com --settings config/settings.json
-scalingo push scalingo-en master
+git push scalingo-en master
 echo "--- Done deploying to q42.com. Refresh your browser!"
-echo
-
-# Put back the public folder
-echo "--- Restoring public folder..."
-mv $TMPDIR/q42nl-deploy-public/* .
 echo
 
 echo "------ Done. ------"
