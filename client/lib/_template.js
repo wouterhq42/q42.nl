0bb7702728e4082e00b9a48751e60f8174e78e32
dierat <diedra.rater@gmail.com>
2015-10-26 16:18:43 -0700

    Refactor _template file from CoffeeScript to EcmaScript

--- client/lib/_template.js
+++ client/lib/_template.js
@@ -1,13 +1,18 @@
-// # Helper to easily construct templates for multiple languages
-// @$Template = (templates)  -> _.each templates, (values, tmpl) ->
-//   _tmpl("helpers", tmpl, values)
-// @$Events = (tmpl, events) -> _tmpl("events", tmpl, events)
-// @$OnRendered = (tmpl, fn) -> _tmpl("onRendered", tmpl, fn)
-//
-// _tmpl = (type, tmpl, obj) ->
-//   Template["en_#{tmpl}"]?[type] obj
-//   Template[tmpl][type] obj
-
-
-
 // Helper to easily construct templates for multiple languages
+$Template = function(templates) {
+  _.each(templates, (values, tmpl) => {
+    _tmpl("helpers", tmpl, values)
+  });
+};
+$Events = function(tmpl, events) {
+  _tmpl("events", tmpl, events);
+};
+$OnRendered = function(tmpl, fn) {
+  _tmpl("onRendered", tmpl, fn);
+};
+
+const _tmpl = function(type, tmpl, obj) {
+  const result = Template["en_" + tmpl];
+  if (result != null) result[type](obj);
+  return Template[tmpl][type](obj);
+};
