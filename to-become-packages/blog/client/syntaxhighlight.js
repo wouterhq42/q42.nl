edbb67940dc70e1a1f68d5be1d9469d4b95534a6
dierat <diedra.rater@gmail.com>
2015-10-27 13:17:55 -0700

    Refactor syntaxhighlight file from CoffeeScript to ECMAScript

---
+++ to-become-packages/blog/client/syntaxhighlight.js
@@ -0,0 +1,19 @@
+syntaxHighlight = () => {
+  let a = false;
+
+  $('code').each(function() {
+    $parent = $(this).parent();
+    if ( !$parent.hasClass("prettyprint") && $parent.is("pre") ){
+      $parent.addClass("prettyprint")
+      a = true;
+    }
+  });
+
+  if (a) prettyPrint();
+}
+
+Template.blog.onRendered(syntaxHighlight);
+Template.blogpost.onRendered(syntaxHighlight);
+if (Template.en_blog){
+  Template.en_blog.onRendered(syntaxHighlight);
+}
