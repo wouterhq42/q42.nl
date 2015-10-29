2f7e9359a7fb8705dbfabc8a054912a82004f2e8
Rahul Choudhury <rchoudhury@gmail.com>
2015-10-29 15:50:52 +0100

    Rewrite to Ecmascript

---
+++ to-become-packages/things/client/template.thing.js
@@ -0,0 +1,24 @@
+
+Template.thing.onCreated(() => {
+  this.thingId = Template.currentData().id;
+  this.autorun( () => this.subscribe("things", [this.thingId]) );
+});
+
+Template.thing.helpers({
+  content() {
+    let thing = Things.findOne({name: Template.instance().thingId});
+    if (thing)
+      return thing.content;
+  }
+});
+
+Template.group.onCreated(() => {
+  this.thingIds = Template.currentData().ids.split(",");
+  this.autorun( () => this.subscribe("things", this.thingIds) );
+});
+
+Template.group.helpers({
+  things() {
+    return Things.find({name: {$in: Template.instance().thingIds}});
+  }
+});
