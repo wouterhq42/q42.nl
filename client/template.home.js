0ff08c0b3a58c5a044b90aae71bdb9bb80d77009
Bram Stege <bramstege@Q42-MacBook-Pro-van-Bram.local>
2015-11-02 11:58:55 +0100

    styling cleanup

--- client/template.home.js
+++ client/template.home.js
@@ -6,5 +6,15 @@ Template.home.helpers({
   isByQer: (authorName) => {
     return (authorName !== 'Rahul Choudhury' && authorName !== 'Q42' &&
         authorName !== 'Ineke Scheffers') ? 'byqer' : '';
+  },
+  num_employees: () => {
+    if (EmployeeCount.findOne()) return EmployeeCount.findOne().count;
+  },
+  post: function() {
+    return blogpostIndex.find({}, {
+      sort: {
+        date: -1
+      }
+    });
   }
 });
--- client/templates.js
+++ client/templates.js
@@ -1,22 +1,6 @@
 $Template({
-  home: {
-    num_employees: () => {
-      if (EmployeeCount.findOne()) return EmployeeCount.findOne().count;
-    },
-    post: function() {
-      return blogpostIndex.find({}, {
-        sort: {
-          date: -1
-        }
-      });
-    }
-  },
   header: {
     headerGameActive: () => Session.equals("headerGameActive", true),
     dev: () => window.location.href.indexOf("localhost") > -1
   }
-});
-
-
-Template.hero.helpers({
 });
