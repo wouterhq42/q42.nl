cf0e831c30234b78de2592887530822378be1440
Arian van Gend <arian@q42.nl>
2015-11-03 13:51:47 +0100

    WIP work detail page.

--- to-become-packages/work/client/client.js
+++ to-become-packages/work/client/client.js
@@ -1,4 +1,14 @@
+Meteor.startup(() => {
+  Meteor.subscribe('work');
+  Meteor.subscribe('things');
+});
+
+function work() {
+  return Work.findOne({ slug: FlowRouter.current().params.slug });
+}
 Template.workDetail.helpers({
-  work: () => Work.findOne(),
+  work: () => work(),
+  qers: () => Employees.find({ handle: { $in: work().properties.qers } }),
+  getThing: (thingId) => Things.findOne({ name: thingId }),
   prettifyDate: (date) => `${date.getMonth()+1}/${date.getFullYear()}`
 });
