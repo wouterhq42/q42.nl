ccbc4615e449d2e61dbfcbbc4fbf14184dae01ca
Rahul Choudhury <rchoudhury@gmail.com>
2015-11-05 10:43:31 +0100

    Swallow errors with jsmpeg initialization

--- to-become-packages/hero/client/hero.js
+++ to-become-packages/hero/client/hero.js
@@ -1,11 +1,14 @@
 Template.hero.onRendered(function() {
-  let client = new WebSocket('ws://floating-everglades-9343.herokuapp.com/');
-  let canvas = document.getElementById('bgvid');
-  let player = new jsmpeg(client, {canvas:canvas});
+  const client = new WebSocket('ws://floating-everglades-9343.herokuapp.com/');
+  const canvas = document.getElementById('bgvid');
+  try {
+    new jsmpeg(client, {canvas:canvas});
+  }
+  catch (e) {}
 });
 
 Template.hero.helpers({
   show: function() {
     return (FlowRouter.getRouteName() === "home");
   }
-})
+});
