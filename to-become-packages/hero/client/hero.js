e9780a153d04f2970ac98516a44b78cc2756b64a
Rahul Choudhury <rchoudhury@gmail.com>
2015-11-06 15:27:40 +0100

    Disable hero stream for now

--- to-become-packages/hero/client/hero.js
+++ to-become-packages/hero/client/hero.js
@@ -1,11 +1,11 @@
-Template.hero.onRendered(function() {
-  const client = new WebSocket('ws://floating-everglades-9343.herokuapp.com/');
-  const canvas = document.getElementById('bgvid');
-  try {
-    new jsmpeg(client, {canvas:canvas});
-  }
-  catch (e) {}
-});
+// Template.hero.onRendered(function() {
+//   const client = new WebSocket('ws://floating-everglades-9343.herokuapp.com/');
+//   const canvas = document.getElementById('bgvid');
+//   try {
+//     new jsmpeg(client, {canvas:canvas});
+//   }
+//   catch (e) {}
+// });
 
 Template.hero.helpers({
   show: function() {
