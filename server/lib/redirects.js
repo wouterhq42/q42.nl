2d6035b944b2b739ac98bd2159d4b49ba3736430
Rahul Choudhury <rchoudhury@gmail.com>
2015-11-10 16:01:41 +0100

    Attempt to fix & refactor redirects

--- server/lib/redirects.js
+++ server/lib/redirects.js
@@ -1,97 +1,48 @@
-// const HTTP_REDIRECT_TEMPORARY = 301;
-// const HTTP_REDIRECT_PERMANENT = 302;
-// const urlsToRedirectToEn = ["/meteor", "/swift", "/interaction-engineering"];
-// const urlsToRedirectToNl = ["/games"];
-// const demoUrls = ["/demos/colorblindnesssimulator", "/demos/contrastcheck"];
-
 // Use Picker middleware to handle server-side routes
 // per https://github.com/meteorhacks/picker/issues/22
 
-// .NL ==> .COM
-// Picker.middleware((req, res, next) => {
-//   if (req.headers.host === "q42.nl"){
-//     if (urlsToRedirectToEn.indexOf(req.url) !== -1){
-//       console.log(`Redirect NL to EN: ${req.url}`);
-//       res.writeHead(HTTP_REDIRECT_PERMANENT, {
-//         Location: `http://q42.com${req.url}`
-//       });
-//       res.end();
-//     }
-//   } else {
-//     next();
-//   }
-// });
+redirect(["/meteor", "/swift", "/interaction-engineering"],
+  "q42.nl", "q42.com");
+redirect(["/games"], "q42.com", "q42.nl");
 
-// .COM ==> .NL
-// Picker.middleware((req, res, next) => {
-//   if (req.headers.host === "q42.com"){
-//     if (urlsToRedirectToEn.indexOf(req.url) !== -1){
-//       console.log(`Redirect EN to NL: ${req.url}`);
-//       res.writeHead(HTTP_REDIRECT_PERMANENT, {
-//         Location: `http://q42.nl${req.url}`
-//       });
-//       res.end();
-//     }
-//   } else {
-//     next();
-//   }
-// });
-//
-// Picker.middleware((req, res, next) => {
-//   if (_.contains(["/accessibility", "/a11y"], req.url)){
-//     res.writeHead(HTTP_REDIRECT_PERMANENT, {
-//       Location: "http://q42.com/interaction-engineering"
-//     });
-//     res.end();
-//
-//   } else if (req.url === "/adventures"){
-//     res.writeHead(HTTP_REDIRECT_TEMPORARY, {
-//       Location: "http://adventures.handcraft.com"
-//     });
-//     res.end();
-//
-//   // SEE extension
-//   } else if (_.contains(demoUrls, req.url)){
-//     res.writeHead(HTTP_REDIRECT_PERMANENT,{
-//       Location: `https://chrome.google.com/webstore/
-//                 detail/see/dkihcccbkkakkbpikjmpnbamkgbjfdcn`
-//     });
-//     res.end();
-//
-//   } else {
-//     next();
-//   }
-// });
+redirect(["/accessibility", "/a11y"], null,
+  "http://q42.com/interaction-engineering");
+redirect(["/adventures"], null, "http://adventures.handcraft.com");
 
-// XXX: make this easier
-// Picker.middleware((req, res, next) => {
-//   const isDotCom = req.headers.host === "q42.com";
-//   if (isDotCom && _.contains("products", req.url)) {
-//     res.writeHead(HTTP_REDIRECT_PERMANENT, {
-//       Location: "http://q42.com/projects"
-//     });
-//     res.end();
-//   }
-//   else if (!isDotCom && _.contains("producten", req.url)) {
-//     res.writehead(HTTP_REDIRECT_PERMANENT, {
-//       Location: "http://q42.nl/projecten"
-//     });
-//     res.end();
-//   } else {
-//     next();
-//   }
-// });
+const demoUrls = ["/demos/colorblindnesssimulator", "/demos/contrastcheck"];
+const seeChromeWebStore = "https://chrome.google.com/webstore/detail/see/" +
+                          "dkihcccbkkakkbpikjmpnbamkgbjfdcn";
+redirect(demoUrls, null, seeChromeWebStore);
 
-// Picker.middleware((req, res, next) => {
-//   const host = req.headers.host;
-//   const fullUrl = `http://${host}${req.url}`;
-//   if (host.indexOf("www") === 0){
-//     console.log(`Route: removeWWW (${req.url})`);
-//     res.writeHead(HTTP_REDIRECT_PERMANENT, {
-//       Location: fullUrl.replace("www.", "")
-//     });
-//     res.end();
-//   } else {
-//     next();
-//   }
-// });
+redirect(["products"], "q42.com", "http://q42.com/projects");
+redirect(["producten"], "q42.nl", "http://q42.nl/projecten");
+
+function redirect(urls, from, to) {
+  const HTTP_REDIRECT_PERMANENT = 302;
+  Picker.middleware((req, res, next) => {
+    const check = (from) => from ? req.headers.host === from : true;
+    if (check() && urls.indexOf(req.url) !== -1) {
+      console.log(`Redirect ${from} to ${to}: ${req.url}`);
+      res.writeHead(HTTP_REDIRECT_PERMANENT, {
+        Location: `http://${to}${req.url}`
+      });
+      res.end();
+    } else {
+      next();
+    }
+  });
+}
+
+Picker.middleware((req, res, next) => {
+  const host = req.headers.host;
+  const fullUrl = `http://${host}${req.url}`;
+  if (host.indexOf("www") === 0){
+    console.log(`Route: removeWWW (${req.url})`);
+    res.writeHead(HTTP_REDIRECT_PERMANENT, {
+      Location: fullUrl.replace("www.", "")
+    });
+    res.end();
+  } else {
+    next();
+  }
+});
