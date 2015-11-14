f3a5ae213cf102a46868a91ad262a25d7e4f76e4
Rahul Choudhury <rchoudhury@gmail.com>
2015-11-14 18:02:01 +0100

    Refactor + cleanup and fix blog paging bug

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -1,20 +1,5 @@
 Utils = {
 
-  // return the correct name of the template
-  // depending on the current language
-  getTemplate: (name) => {
-    const enName = `en_${name}`;
-    if ( Session.equals("lang", "en") && Template[enName] ){
-      return enName;
-    } else if ( Session.equals("lang", "en") && !Template[enName] ){
-      return "error404";
-    } else if ( Session.equals("lang", "nl") && !Template[name] ){
-      return "error404";
-    } else {
-      return name;
-    }
-  },
-
   // return the pages to be displayed as pagination on the blog
   getPagination: (pageNum, tag) => {
     pageNum = pageNum * 1;
@@ -27,20 +12,15 @@ Utils = {
 
     if (pages !== 1) {
       const page = pageNum || 1;
-      if (page > 1) {
-        items.push({ label: newer, page: page - 1 });
-      }
-
       const min = Math.max(1, page - 3);
       const max = Math.min(pages, page + 3);
 
-      for (let i = min; i <= max; i++) {
-        items.push({ label: i, pagex: i, active: i === page });
-      }
+      if (page > 1) items.push({ label: newer, page: page - 1 });
 
-      if (page < pages) {
-        items.push({ label: older, page: page + 1 });
-      }
+      for (let i = min; i <= max; i++)
+        items.push({ label: i, page: i, active: i === page });
+
+      if (page < pages) items.push({ label: older, page: page + 1 });
     }
 
     return items;
