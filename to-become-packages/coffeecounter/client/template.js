32cd86ba40be044b1b6c9f714f54b374c90dda55
Rahul Choudhury <rchoudhury@gmail.com>
2015-12-31 15:13:33 +0100

    Update live counter for cups of coffee with sparkline

--- to-become-packages/coffeecounter/client/template.js
+++ to-become-packages/coffeecounter/client/template.js
@@ -1,5 +1,17 @@
+const CoffeeCups = new Mongo.Collection("coffeecups");
+
 Template.numCupsOfCoffee.helpers({
-  numCupsOfCoffee: () => {
-    return Counts.get("coffeeCups");
+  numCupsOfCoffee: () => Counts.get("coffeeCups")
+});
+
+Template.numCupsOfCoffeeSparkline.helpers({
+  sparklineData() {
+    const cups = CoffeeCups.findOne();
+    if (cups)
+      return cups.values;
   }
+});
+
+Template.numCupsOfCoffeeSparkline.onCreated(function() {
+  this.subscribe("coffeeCounterHistory");
 });
--- to-become-packages/coffeecounter/server/coffeecounter.js
+++ to-become-packages/coffeecounter/server/coffeecounter.js
@@ -1,10 +1,25 @@
-Meteor.startup( () => {
-  const iotApi = DDP.connect("https://iot-api.scalingo.io");
-  const CoffeeCups = new Mongo.Collection("coffeecups", iotApi);
+const iotApi = DDP.connect("https://iot-api.scalingo.io");
+const CoffeeCups = new Mongo.Collection("coffeecups", iotApi);
 
-  Meteor.publish("coffeeCounter", function() {
-    const yesterday = new Date(new Date().setHours(0,0,0,0)).toISOString();
-    Counts.publish(this, "coffeeCups",
-      CoffeeCups.find({published_at: {$gt: yesterday}}));
+Meteor.publish("coffeeCounter", function() {
+  const yesterday = new Date(new Date().setHours(0,0,0,0)).toISOString();
+  Counts.publish(this, "coffeeCups",
+    CoffeeCups.find({published_at: {$gt: yesterday}}));
+});
+
+Meteor.publish("coffeeCounterHistory", function() {
+  const threeWeeks = 1000 * 60 * 60 * 24 * 21;
+  let threeWeeksAgo = new Date().setHours(0,0,0,0) - threeWeeks;
+  threeWeeksAgo = new Date(threeWeeksAgo).toISOString();
+  const cups = CoffeeCups.find({published_at: {$gt: threeWeeksAgo}},
+    {fields: {published_at: 1}});
+
+  const grouped = _.groupBy(cups.fetch(), (doc) => {
+    return +new Date(doc.published_at).setHours(0,0,0,0);
   });
+  const result = _.map(grouped, arr => arr.length).join(",");
+
+  // XXX: make this reactive to the above cursor if we want it to be live
+  this.added("coffeecups", null, { values: result });
+  this.ready();
 });
