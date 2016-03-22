c39874b07ebd8e7f9f7030ac0dfa035494d7f254
Rahul Choudhury <rchoudhury@gmail.com>
2016-03-22 09:53:08 +0100

    Remove mongo url :(

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export MONGO_URL=mongodb://q42-content:NftPRP4LeZtgt7@lamppost.15.mongolayer.com:10237,lamppost.14.mongolayer.com:10260/q42-content?replicaSet=set-561d18a8c5d0c2f3ea00111e
 meteor --settings config/settings.json
