1f8b4e38cae3cf694a3b2270f8127b624aecb7e3
Rahul Choudhury <rchoudhury@gmail.com>
2016-04-06 13:18:07 +0200

    Fix indentation

--- client/template.main.js
+++ client/template.main.js
@@ -5,11 +5,11 @@ import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Utils } from '../lib/utils'
 
 Template.main.helpers({
-    header: () => Utils.getSiteVersion() === "en" ? "en_header" : "header",
-    footer: () => Utils.getSiteVersion() === "en" ? "en_footer" : "footer",
-    openChat: () => Session.equals("openChat", true),
-    visible: () => "visible-backgrounds",
-    inverted() {
-      return (FlowRouter.getRouteName() === 'home') ? 'inverted' : '';
-    }
+  header: () => Utils.getSiteVersion() === "en" ? "en_header" : "header",
+  footer: () => Utils.getSiteVersion() === "en" ? "en_footer" : "footer",
+  openChat: () => Session.equals("openChat", true),
+  visible: () => "visible-backgrounds",
+  inverted() {
+    return (FlowRouter.getRouteName() === 'home') ? 'inverted' : '';
+  }
 });
