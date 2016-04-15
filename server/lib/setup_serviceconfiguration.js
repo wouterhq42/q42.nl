94bcb8b907d679c7accdf6f10b23109c44d4e48f
Rahul Choudhury <rchoudhury@gmail.com>
2016-04-15 23:37:41 +0200

    Add imports

--- server/lib/setup_serviceconfiguration.js
+++ server/lib/setup_serviceconfiguration.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor'
+import { ServiceConfiguration } from 'meteor/service-configuration'
+
 Meteor.startup(() => {
   _.each(Meteor.settings.private.serviceConfiguration, (settings) => {
     settings = _.extend(settings, {loginStyle: "popup"});
