7a3e57bd5111a940c132aaabfb2fbe9a591041a8
Rahul Choudhury <rchoudhury@gmail.com>
2016-04-21 11:15:19 +0200

    Fix logo to be interactive and add modules

--- client/template.header.js
+++ client/template.header.js
@@ -1,8 +1,19 @@
 import { $ } from 'meteor/jquery'
-import { $Events } from './lib/_template'
+import { $Events, $Helpers } from './lib/_template'
 
 $Events("header", {
   "click #mobile-menu-icon": () => $("body").toggleClass("show-mobile-menu"),
   "focus li a":              () => $("body").addClass("show-mobile-menu"),
   "click li a":              () => $("body").removeClass("show-mobile-menu")
+});
+
+$Helpers({
+  header: {
+    randomRenderer() {
+      return _.chain([
+        "webgl-freak",
+        "webgl-wave"
+      ]).shuffle().first().value();
+    }
+  }
 });
